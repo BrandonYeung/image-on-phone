4810498b2a9fe0bbad478094f2e0d01fd7eec39b
BrandonYeung <47640783+BrandonYeung@users.noreply.github.com>
2019-02-14 12:42:06 -0500

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,13 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+
+-- Set default screen background color to blue
+display.setDefault( "background", 0, 0, 1 )
+
+-- note that the image is saved in a directory C:\Users\s306742\Downloads\Unit2-02\Unit2-02
+local image = display.newImageRect("Jump (4).png", 500, 500)
+image.x = 200
+image.y = 200
